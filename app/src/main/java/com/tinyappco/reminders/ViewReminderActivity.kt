7ce03884111883566fbe5dbcdeb25fa4a4f69c43
Andrew <a.muncey@chester.ac.uk>
2020-09-30 15:47:43 +0100

    update to api 30, migrate to androidx

--- app/src/main/java/com/tinyappco/reminders/ViewReminderActivity.kt
+++ app/src/main/java/com/tinyappco/reminders/ViewReminderActivity.kt
@@ -1,6 +1,6 @@
 package com.tinyappco.reminders
 
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import kotlinx.android.synthetic.main.activity_view_reminder.*
 
